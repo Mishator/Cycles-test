d8664f0af9ce606973ccbdab017523e08fce221a
Mishator <voyteh89@mail.ru>
2023-03-11 16:25:57 +0300

    Сделал подсчёт с годовым процентом.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,25 @@
 public class Main {
     public static void main(String[] args) {
+        task1();
+        task2();
+    }
+    public static void task1 () {
+        System.out.println("Задача 1");
         for (int i = 0; i < 10; i++) {
             System.out.println("Итерация цикла " + i);
         }
+        System.out.println();
+    }
+    public static void task2 () {
+        System.out.println("Задача 2");
+        int salary = 65535;
+        int total = 0;
+        for (int i = 0; i < 12; i++) {
+            total = total + total/100;
+            total = total + salary;
+            System.out.println("Месяц " + i + " Итого " + total);
+        }
+        System.out.println(total);
+        System.out.println();
     }
 }
