4be45ad1acbafb557c1b08d931805b10b681b80d
Mishator <voyteh89@mail.ru>
2023-03-14 19:53:10 +0300

    Выполнил задачу по массивам.

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
         task2();
         task3();
         task4();
+        task5();
     }
     public static void task1 () {
         System.out.println("Задача 1");
@@ -45,5 +46,21 @@ public class Main {
             System.out.println(total);
         }
         System.out.println();
+    }
+
+    public static void task5() {
+        System.out.println("Задача 5");
+        int [] weights ={90, 91, 93, 92, 85, 87, 88, 89, 0, 0, 0, 0};
+        weights[0] = 90;
+        int januaryWeight = weights[0];
+        System.out.println(januaryWeight);
+        System.out.println(weights[0]);
+        System.out.println(weights[4]);
+        int january = 0;
+        System.out.println(weights[january]);
+        for (int i = 0; i < weights.length; i++) {
+            System.out.println(weights[i]);
+
+        }
     }
     }
